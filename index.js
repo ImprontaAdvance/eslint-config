83958385f32c51674238535f3421b678f0276aec
Matteo Manchi <matteo.manchi@gmail.com>
2017-08-21 16:44:33 +0200

    Auto-format eslint-config itself
    Make eslint pretty again

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:flowtype/recommended",
-    "prettier",
-    "prettier/flowtype",
-    "prettier/react"
-  ],
-  plugins: ["react", "jsx-a11y", "import", "flowtype", "prettier"],
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        singleQuote: true,
-        trailingComma: "all",
-        bracketSpacing: false,
-        jsxBracketSameLine: true,
-        parser: "flow",
-        tabWidth: 4
-      }
-    ]
-  },
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: "module",
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-      jsx: true
-    }
-  },
-  env: {
-    es6: true,
-    node: true
-  }
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:flowtype/recommended',
+        'prettier',
+        'prettier/flowtype',
+        'prettier/react',
+    ],
+    plugins: ['react', 'jsx-a11y', 'import', 'flowtype', 'prettier'],
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                singleQuote: true,
+                trailingComma: 'all',
+                bracketSpacing: false,
+                jsxBracketSameLine: true,
+                parser: 'flow',
+                tabWidth: 4,
+            },
+        ],
+    },
+    parserOptions: {
+        ecmaVersion: 2017,
+        sourceType: 'module',
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+            jsx: true,
+        },
+    },
+    env: {
+        es6: true,
+        node: true,
+    },
 };
