ea0583d3c61674270b6e18cba930e0c4578380be
Matteo Manchi <matteo.manchi@gmail.com>
2019-05-06 19:05:56 +0200

    New eslint based on eslint-create-react-app

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 module.exports = {
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:flowtype/recommended',
-        'prettier',
-        'prettier/flowtype',
-        'prettier/react',
-    ],
-    plugins: ['react', 'jsx-a11y', 'import', 'flowtype', 'prettier'],
+    extends: ['react-app', 'prettier', 'prettier/flowtype', 'prettier/react'],
+    plugins: ['prettier'],
     rules: {
         'prettier/prettier': [
             'error',
@@ -15,25 +8,11 @@ module.exports = {
                 singleQuote: true,
                 trailingComma: 'es5',
                 bracketSpacing: false,
-                jsxBracketSameLine: true,
                 parser: 'flow',
-                tabWidth: 4,
             },
         ],
         'block-scoped-var': 'warn',
         camelcase: 'warn',
         curly: ['error', 'all'],
-    },
-    parserOptions: {
-        ecmaVersion: 2017,
-        sourceType: 'module',
-        ecmaFeatures: {
-            experimentalObjectRestSpread: true,
-            jsx: true,
-        },
-    },
-    env: {
-        es6: true,
-        node: true,
     },
 };
