12dbb46bd11855a5ac69284ce381f9b094bd453b
Matteo Manchi <matteo.manchi@gmail.com>
2019-05-06 19:32:27 +0200

    New eslint based on eslint-create-react-app (#15)
    * New eslint based on eslint-create-react-app
    
    * index.js linted with new rules
    
    * Update README with new peer dependencies

--- index.js
+++ index.js
@@ -1,39 +1,18 @@
 module.exports = {
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:flowtype/recommended',
-        'prettier',
-        'prettier/flowtype',
-        'prettier/react',
+  extends: ['react-app', 'prettier', 'prettier/flowtype', 'prettier/react'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+        bracketSpacing: false,
+        parser: 'flow',
+      },
     ],
-    plugins: ['react', 'jsx-a11y', 'import', 'flowtype', 'prettier'],
-    rules: {
-        'prettier/prettier': [
-            'error',
-            {
-                singleQuote: true,
-                trailingComma: 'es5',
-                bracketSpacing: false,
-                jsxBracketSameLine: true,
-                parser: 'flow',
-                tabWidth: 4,
-            },
-        ],
-        'block-scoped-var': 'warn',
-        camelcase: 'warn',
-        curly: ['error', 'all'],
-    },
-    parserOptions: {
-        ecmaVersion: 2017,
-        sourceType: 'module',
-        ecmaFeatures: {
-            experimentalObjectRestSpread: true,
-            jsx: true,
-        },
-    },
-    env: {
-        es6: true,
-        node: true,
-    },
+    'block-scoped-var': 'warn',
+    camelcase: 'warn',
+    curly: ['error', 'all'],
+  },
 };
