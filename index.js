d21345924df24d0c1f756ec28f68eee83c8917a8
Matteo Manchi <matteo.manchi@gmail.com>
2019-05-06 19:08:58 +0200

    index.js linted with new rules

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 module.exports = {
-    extends: ['react-app', 'prettier', 'prettier/flowtype', 'prettier/react'],
-    plugins: ['prettier'],
-    rules: {
-        'prettier/prettier': [
-            'error',
-            {
-                singleQuote: true,
-                trailingComma: 'es5',
-                bracketSpacing: false,
-                parser: 'flow',
-            },
-        ],
-        'block-scoped-var': 'warn',
-        camelcase: 'warn',
-        curly: ['error', 'all'],
-    },
+  extends: ['react-app', 'prettier', 'prettier/flowtype', 'prettier/react'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+        bracketSpacing: false,
+        parser: 'flow',
+      },
+    ],
+    'block-scoped-var': 'warn',
+    camelcase: 'warn',
+    curly: ['error', 'all'],
+  },
 };
